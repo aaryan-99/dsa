411f789b03d747ae0a9a0b81c8bd9072d301cc43
aaryan_99 <3999.gouraaryan.xa@gmail.com>
2025-10-20 21:40:12 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 public class Main{
-    public static void main(String[] args) {
-        System.out.println("This code has been posted on git hub");
+    static void changeString(String a){
+        a = "Kanishk";
+        System.out.println("new string is : " +a);
     }
-}
+    public static void main(String[] args) {
+        String a = "Arnav";
+        changeString(a);
+        System.out.println(a);
+    }
+    }
