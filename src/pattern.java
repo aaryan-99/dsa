498b6bbc8cef52d2ba082c2e8a45fe5f487cb5da
aaryan_99 <3999.gouraaryan.xa@gmail.com>
2025-10-21 17:02:25 +0530

    add file

---
+++ src/pattern.java
@@ -0,0 +1,31 @@
+public class pattern{
+    public static void main(String[] args) {
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9-i-1; j++){
+                System.out.print(" ");
+            }
+            for(int k=0; k<2*i+1; k++){
+                System.out.print("*");
+            }
+            for(int j=0; j<9-i-1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for(int i=0; i<9; i++){
+            for(int j=0; j<i; j++){
+                System.out.print(" ");
+            }
+            for(int k=0; k<2*9-(2*i+1); k++){
+                System.out.print("*");
+            }
+            for(int j=0; j<i; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
