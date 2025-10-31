9942f6bacc05ceeb34e6746820158f43802448ab
aaryan_99 <3999.gouraaryan.xa@gmail.com>
2025-10-31 12:19:21 +0530

    Update quick_sort.java

--- src/quick_sort.java
+++ src/quick_sort.java
@@ -1,44 +1,36 @@
 public class quick_sort {
-    static int partitionindex(int[]arr, int low, int high){
-        int pivot = arr[0];
-        for(int i = 0; i<high-1; i++){
-            for(int j = i+1; j<high; j++){
-            if(pivot>=arr[j]){
-                int temp = arr[j];
-                arr[j]=pivot;
-                pivot=temp;
-            }}
-        }
-        int pivotindex=0;
-        for(int i = 0; i<high; i++){
-            if(arr[i]==pivot){
-                 pivotindex=i;
-            }
-        }
-        return pivotindex;
+    static int partition(int[] arr, int low, int high){
+       int pivot = arr[low];
+       int left = low + 1;
+       int right = high;
+       while(left<=right){
+           while(left<=right && arr[left]<pivot) left++;
+           while(left<=right && arr[right]>pivot) right--;
+           if(left<right){
+               int temp = arr[left];
+               arr[left] = arr[right];
+               arr[right] = temp;
+           }}
+           int temp = arr[right];
+           arr[right] = arr[low];
+           arr[low] = temp;
+            return right;
+    }
+    static void Sort(int[] arr, int low, int high){
+        if(low>=high) return;  // base case....
+        int pivotIndex = partition(arr, low, high);
+        // recursion...
+       Sort(arr, low, pivotIndex-1);
+       Sort(arr, pivotIndex+1, high);
     }
 
-        // Function to perform quicksort
-      static void quickSort(int[] arr, int low, int high) {
-            // Base case
-            if (low < high) {
-                // Find pivot index
-                int pivotIndex = partitionindex(arr, low, high);
 
-                // Sort left subarray
-                quickSort(arr, low, pivotIndex - 1);
-
-                // Sort right subarray
-                quickSort(arr, pivotIndex + 1, high);
-            }
-        }
 
     public static void main(String[] args) {
-        int [] arr = {13,4,20,45,28,1,2,9};
-        quickSort(arr,0, arr.length-1);
-        System.out.println("array is:");
+        int[] arr = {13,4,20,45,28,1,2,9};
+        Sort(arr, 0, 7);
+        System.out.println("Array after sorting is:");
         for(int a: arr){
             System.out.print(a+" ");
-        }
     }
-}
+}}
